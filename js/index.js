57e428d5f3b98605344909f48f51b73e8864f9f5
juliaCampos <julia.xavier.campos@gmail.com>
2024-11-23 20:27:54 -0300

    just starting

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/* modos de exibição de js no html */
+document.getElementById("texto").innerHTML="Lorem Ipsum <b>texto por js </b>";
+alert("modal de alerta");
+console.log("era pra aparecer esse console.log");
